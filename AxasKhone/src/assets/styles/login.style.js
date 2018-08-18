93c0e1800b05709efe11ed07a013d86bf7078b85
samy <samprada3@gmail.com>
2018-08-18 13:31:56 +0430

    add input and button

--- AxasKhone/src/assets/styles/login.style.js
+++ AxasKhone/src/assets/styles/login.style.js
@@ -4,17 +4,25 @@ const styles = StyleSheet.create({
     container: {
       flex: 1,
       // tooli mindaze vasat
-      justifyContent: 'center',
+    //  justifyContent: 'center',
       // arzi mindaze vasat
-      alignItems: 'center',
+    //  alignItems: 'center',
       backgroundColor: 'rgb(92, 92, 92)'
     },
-    welcome: {
+    title: {
       fontSize: 20,
       textAlign: 'center',
-      margin: 10,
-      color: 'white'
+      //margin-top: 10,
+      color: 'rgb(240, 240, 240)',
+      paddingTop: 150
     },
+    inputText: {
+        color: 'black',
+        padding: 5,
+        margin: 10,
+        backgroundColor: 'white'
+
+    }
   });
 
   export default styles;
--- AxasKhone/src/components/Login.js
+++ AxasKhone/src/components/Login.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, TextInput, Button} from 'react-native';
 import styles from '../assets/styles/login.style'
 
 type Props = {};
@@ -7,7 +7,21 @@ export default class Login extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>hi gogoli :))</Text>
+        <Text style={styles.title}>عکاسخانه</Text>
+        <TextInput 
+        style={styles.inputText} 
+        placeholder="آدرس ایمیل" 
+        underlineColorAndroid="transparent"
+        />
+        <TextInput 
+        style={styles.inputText} 
+        placeholder="رمز عبور" 
+        underlineColorAndroid="transparent"
+        />
+        <Button
+        title="ورود"
+        color="red"
+        />
       </View>
     );
   }
