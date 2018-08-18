c5665d101dd68bc630a28a12dc63b25e8e28a001
samy <samprada3@gmail.com>
2018-08-18 15:54:22 +0430

    styling to the finished

--- AxasKhone/src/assets/styles/login.style.js
+++ AxasKhone/src/assets/styles/login.style.js
@@ -6,23 +6,56 @@ const styles = StyleSheet.create({
       // tooli mindaze vasat
     //  justifyContent: 'center',
       // arzi mindaze vasat
-    //  alignItems: 'center',
+      alignItems: 'center',
       backgroundColor: 'rgb(92, 92, 92)'
     },
     title: {
       fontSize: 20,
       textAlign: 'center',
-      //margin-top: 10,
+      marginBottom: 20,
       color: 'rgb(240, 240, 240)',
-      paddingTop: 150
+      paddingTop: 10
     },
     inputText: {
+        height: 50,
+        width: 320,
         color: 'black',
         padding: 5,
-        margin: 10,
-        backgroundColor: 'white'
+        margin: 5,
+        paddingLeft: 10,
+        paddingRight: 10,
+        backgroundColor: 'white',
+        textAlign: "right",
+        borderRadius: 5,
+    },
 
+    loginButton: {
+        height: 50,
+        width: 320,
+        backgroundColor: 'rgb(182, 182, 182)',
+        color: "white",
+        fontSize: 20,
+        borderRadius: 10,
+        marginTop: 5,
+        textAlign: 'center',
+        padding: 10,
+    },
+
+    forgetPass: {
+        marginTop: 10,
+        color: "white",
+    },
+    
+    logo: {
+        height: 70,
+        width: 70,
+        backgroundColor: 'black',
+        marginTop: 70,
+      //  color: "white",
+        borderRadius: 35
     }
+
+
   });
 
   export default styles;
--- AxasKhone/src/components/Login.js
+++ AxasKhone/src/components/Login.js
@@ -1,5 +1,11 @@
 import React, {Component} from 'react';
-import {Text, View, TextInput, Button} from 'react-native';
+import {Text, 
+        View, 
+        TextInput,
+        Button,
+        TouchableOpacity,
+        Image,
+    } from 'react-native';
 import styles from '../assets/styles/login.style'
 
 type Props = {};
@@ -7,21 +13,33 @@ export default class Login extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.title}>عکاسخانه</Text>
+        <View style={styles.logo}> 
+         <Image borderRadius={35} source={require('../assets/img/logo.jpg')} />
+        </View>
+        <Text style={styles.title}>عکاسخونه</Text>
         <TextInput 
-        style={styles.inputText} 
-        placeholder="آدرس ایمیل" 
-        underlineColorAndroid="transparent"
+          style={styles.inputText} 
+          placeholder="آدرس ایمیل" 
+          underlineColorAndroid="transparent"
         />
         <TextInput 
-        style={styles.inputText} 
-        placeholder="رمز عبور" 
-        underlineColorAndroid="transparent"
+          style={styles.inputText} 
+          placeholder="رمز عبور" 
+          secureTextEntry={true}
+          underlineColorAndroid="transparent"
         />
-        <Button
-        title="ورود"
-        color="red"
-        />
+        <TouchableOpacity>
+            <Text style={styles.loginButton}>ورود</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+            <Text style={styles.forgetPass}>رمز عبور خود را فراموش کرده اید؟</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+            <Text style={[styles.loginButton, {marginTop: 70}]}>ورود با حساب گوگل</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+            <Text style={styles.forgetPass}>ثبت نام</Text>
+        </TouchableOpacity>
       </View>
     );
   }
