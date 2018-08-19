fb57dabceee83cc87a1cf06bbeaef088dbaa9898
samy <samprada3@gmail.com>
2018-08-19 10:02:18 +0430

    stable style login

--- AxasKhone/src/assets/styles/login.style.js
+++ AxasKhone/src/assets/styles/login.style.js
@@ -1,61 +1,58 @@
 import { StyleSheet } from 'react-native';
 
-const styles = StyleSheet.create({
+export default styles = StyleSheet.create({
+    
     container: {
       flex: 1,
-      // tooli mindaze vasat
-    //  justifyContent: 'center',
-      // arzi mindaze vasat
-      alignItems: 'center',
-      backgroundColor: 'rgb(92, 92, 92)'
-    },
-    title: {
-      fontSize: 20,
-      textAlign: 'center',
-      marginBottom: 20,
-      color: 'rgb(240, 240, 240)',
-      paddingTop: 10
-    },
-    inputText: {
-        height: 50,
-        width: 320,
-        color: 'black',
-        padding: 5,
-        margin: 5,
-        paddingLeft: 10,
-        paddingRight: 10,
-        backgroundColor: 'white',
-        textAlign: "right",
-        borderRadius: 5,
+      // backgroundColor: 'rgb(92, 92, 92)'
     },
 
-    loginButton: {
-        height: 50,
-        width: 320,
-        backgroundColor: 'rgb(182, 182, 182)',
-        color: "white",
-        fontSize: 20,
-        borderRadius: 10,
+    item: {
+        flex: 3,
+        // backgroundColor: 'red',
+        // alignItems: 'center', ino age bzari dg text baxa arzeshun kamtrin halat mishe
+        justifyContent: 'flex-end'
+      },
+
+    linearGradient: {
+      flex: 1,
+    },
+
+    title: {
+      fontSize: 20,
+      marginTop: 5,
+      color: 'rgb(240, 240, 240)',
+    //  fontFamily: 'IranNastaliq',
+    },
+
+    inputText: {
+     //   height: '30%',
+        color: 'black',
+        paddingHorizontal: 10,
         marginTop: 5,
-        textAlign: 'center',
+        marginHorizontal: 15,
+        backgroundColor: 'white',
+        borderRadius: 5,
+        textAlign: "right",
         padding: 10,
     },
 
-    forgetPass: {
-        marginTop: 10,
-        color: "white",
+    loginButton: { 
+    //  height: '20%',      
+      backgroundColor: 'rgb(182, 182, 182)',
+      color: "white",
+      fontSize: 20,
+      borderRadius: 10,
+      marginTop: 5,
+      textAlign: 'center',
+      marginHorizontal: 15,
+      padding: 10,
     },
-    
-    logo: {
-        height: 70,
-        width: 70,
-        backgroundColor: 'black',
-        marginTop: 70,
-      //  color: "white",
-        borderRadius: 35
-    }
 
+    forgetPass: {
+      marginTop: 10,
+      color: "white",
+      textAlign: 'center',
+    },
 
   });
-
-  export default styles;
--- AxasKhone/src/components/Login.js
+++ AxasKhone/src/components/Login.js
@@ -1,48 +1,63 @@
 import React, {Component} from 'react';
+import LinearGradient from 'react-native-linear-gradient';
+
 import {Text, 
         View, 
         TextInput,
-        Button,
         TouchableOpacity,
         Image,
-        ScrollView,
-    } from 'react-native';
+        StatusBar,
+        } from 'react-native';
+
+
 import styles from '../assets/styles/login.style'
 
 export default class Login extends Component {
   render() {
     return (
-      <ScrollView contentContainerStyle={styles.contentContainer}>
+      <LinearGradient colors={['rgb(0, 100, 130)', 'rgb(0, 100, 130)', 'rgb(150, 50, 160)']} style={styles.linearGradient}>
       <View style={styles.container}>
-        <View style={styles.logo}> 
-         <Image borderRadius={35} source={require('../assets/img/logo.jpg')} />
+
+        <StatusBar
+          backgroundColor='rgb(0, 100, 130)'
+        />
+
+        <View style={[styles.item, style={alignItems: 'center'}]}>
+          <Image borderRadius={35} source={require('../assets/img/logo.jpg')} />
+          <Text style={styles.title}>عکاسخونه</Text>
         </View>
-        <Text style={styles.title}>عکاسخونه</Text>
-        <TextInput 
-          style={styles.inputText} 
-          placeholder="آدرس ایمیل" 
-          underlineColorAndroid="transparent"
-        />
-        <TextInput 
-          style={styles.inputText} 
-          placeholder="رمز عبور" 
-          secureTextEntry={true}
-          underlineColorAndroid="transparent"
-        />
-        <TouchableOpacity>
-            <Text style={styles.loginButton}>ورود</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-            <Text style={styles.forgetPass}>رمز عبور خود را فراموش کرده اید؟</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-            <Text style={[styles.loginButton, {marginTop: 70}]}>ورود با حساب گوگل</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-            <Text style={[styles.forgetPass, {marginBottom: 20}]}>ثبت نام</Text>
-        </TouchableOpacity>
+
+        <View style={[styles.item, {justifyContent: 'center',flex: 5}]}>
+          <TextInput 
+            style={styles.inputText} 
+            placeholder="آدرس ایمیل" 
+            underlineColorAndroid="transparent"
+          />
+          <TextInput 
+            style={styles.inputText} 
+            placeholder="رمز عبور" 
+            secureTextEntry={true}
+            underlineColorAndroid="transparent"
+          />
+          <TouchableOpacity activeOpacity={.8}>
+              <Text style={styles.loginButton}>ورود</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+              <Text style={styles.forgetPass}>رمز عبور خود را فراموش کرده اید؟</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style={[styles.item, {justifyContent: 'flex-end'}]}>
+          <TouchableOpacity activeOpacity={.8}>
+              <Text style={[styles.loginButton, {}]}>ورود با حساب گوگل</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+              <Text style={[styles.forgetPass, {paddingBottom: 15}]}>ثبت نام</Text>
+          </TouchableOpacity>
+        </View>
+
       </View>
-      </ScrollView>
+      </LinearGradient>
     );
   }
 }
