eb451bdc77c01d747aa118299986ed32a2fb33cf
Ehsan Maders <ehsantitsh@gmail.com>
2018-08-23 21:23:28 +0430

    TFIVE-58| setting up store settings and redux-thunk with reducers.

---
+++ AxasKhone/src/helpers/store.js
@@ -0,0 +1,14 @@
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { authentication } from '../reducers/auth';
+import { users } from '../reducers/user';
+
+const store = createStore(
+  combineReducers({
+    user: users,
+    auth: authentication
+  }),
+  applyMiddleware(thunkMiddleware)
+);
+
+export default store;
