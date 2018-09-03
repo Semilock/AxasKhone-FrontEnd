35855fbbe00cac1321d347ae038080d1251daa30
samy <samprada3@gmail.com>
2018-09-03 10:41:44 +0430

    simple swap scrool implement

--- AxasKhone/src/assets/pages/HomeUser/index.js
+++ AxasKhone/src/assets/pages/HomeUser/index.js
@@ -1,34 +1,65 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import React from 'react';
 
-export default class HomeUser extends Component {
-  static navigationOptions = {
-    tabBarLabel: ({ tintColor }) => (
-      <Text style={{ color: tintColor, textAlign: 'center', marginBottom: 12 }}>
-        home user
-      </Text>
-    )
+import { Text, View, Dimensions, Image, Animated } from 'react-native';
+
+import SlidingUpPanel from 'rn-sliding-up-panel';
+
+export default class HomeUser extends React.Component {
+  static defaultProps = {
+    draggableRange: {
+      top: height / 1.25,
+      bottom: 160
+    }
   };
+
+  _draggedValue = new Animated.Value(-120);
 
   render() {
     return (
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: 'rgb(239, 239, 239)',
-          justifyContent: 'center'
-        }}
-      >
-        <Text
-          style={{
-            textAlign: 'center',
-            Color: 'rgb(57, 57, 57)',
-            fontSize: 24
-          }}
+      <View style={styles.container}>
+        <Text>Hello world</Text>
+        <SlidingUpPanel
+          visible
+          startCollapsed
+          showBackdrop={false}
+          ref={c => (this._panel = c)}
+          draggableRange={this.props.draggableRange}
+          onDrag={v => this._draggedValue.setValue(v)}
         >
-          under develop :))
-        </Text>
+          <View style={styles.panel}>
+            <View style={styles.panelHeader}>
+              <Text style={{ color: '#FFF' }}>Bottom Sheet Peek</Text>
+            </View>
+            <View>
+              <Text style={{ color: '#FFF' }}>
+                Bottom Sheet Cbbbbbbbbbbbbbbbbbbbbbbontent
+              </Text>
+            </View>
+          </View>
+        </SlidingUpPanel>
       </View>
     );
   }
 }
+
+const { height } = Dimensions.get('window');
+
+const styles = {
+  container: {
+    flex: 1,
+    backgroundColor: 'yellow',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  panel: {
+    flex: 1,
+    backgroundColor: 'black',
+    position: 'relative'
+  },
+  panelHeader: {
+    height: 30,
+    backgroundColor: '#b197fc',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+};
