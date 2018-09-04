43fbdeaeb6e33e8e91d4a060c803292aac367e23
Ehsan Maders <ehsantitsh@gmail.com>
2018-09-04 16:19:56 +0430

    clean navigation and split photo page and favirites

--- AxasKhone/src/assets/pages/Profile/ProfileTab.js
+++ AxasKhone/src/assets/pages/Profile/ProfileTab.js
@@ -1,127 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
   TabNavigator,
   TabBarBottom,
   FooterTabs,
   TXTabBar
 } from 'react-navigation';
-import { View, Text, FlatList, Image } from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
-
-class Photo extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      posts: [
-        {
-          imageUrl: './../../img/logo.jpg'
-        },
-        {
-          imageUrl: './../../img/id1.jpg'
-        },
-        {
-          imageUrl: './../../img/logo.jpg'
-        },
-        {
-          imageUrl: './../../img/logo.jpg'
-        },
-        {
-          imageUrl: './../../img/logo.jpg'
-        },
-        {
-          imageUrl: './../../img/id1.jpg'
-        },
-        {
-          imageUrl: './../../img/logo.jpg'
-        },
-        {
-          imageUrl: './../../img/logo.jpg'
-        },
-        {
-          imageUrl: './../../img/logo.jpg'
-        },
-        {
-          imageUrl: './../../img/id1.jpg'
-        },
-        {
-          imageUrl: './../../img/logo.jpg'
-        },
-        {
-          imageUrl: './../../img/logo.jpg'
-        }
-      ],
-      exp: [{ key: 'a' }, { key: 'b' }]
-    };
-  }
-
-  render() {
-    return (
-      <View style={{ flex: 1 }}>
-        {/* <FlatList
-          data={this.state.exp}
-          renderItem={({item}) => 
-          <Text>{item.key}</Text>}
-        /> */}
-
-        <FlatList
-          data={this.state.posts}
-          numColumns={2}
-          renderItem={item => (
-            <View style={{ flex: 1, justifyContent: 'space-around' }}>
-              <View
-                style={{
-                  flex: 0.5,
-                  borderWidth: 5,
-                  height: 150,
-                  borderColor: 'white'
-                }}
-              >
-                {/* <Image resizeMode="stretch" source={require('./../../img/id1.jpg')} /> */}
-              </View>
-            </View>
-          )}
-          // renderItem={(item) => <Image source={{uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png'}} />}
-          // <Text>{item.imageUrl}</Text>}
-        />
-      </View>
-    );
-  }
-}
-
-// class FlatListItem extends React.Component {
-//   render(){
-
-//   }
-// }
-
-class Favotites extends React.Component {
-  render() {
-    return (
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: 'rgb(239, 239, 239)',
-          justifyContent: 'center'
-        }}
-      >
-        <Text
-          style={{
-            textAlign: 'center',
-            Color: 'rgb(57, 57, 57)',
-            fontSize: 24
-          }}
-        >
-          hi :))
-        </Text>
-      </View>
-    );
-  }
-}
+import { Text } from 'react-native';
+import Photo from './ProfilePosts';
+import Favorites from './ProfileFavorites';
 
 export default (AppStackNavigator = TabNavigator(
   {
-    Favotites: {
-      screen: Favotites,
+    Favorites: {
+      screen: Favorites,
       navigationOptions: {
         tabBarLabel: ({ tintColor }) => (
           <Text
