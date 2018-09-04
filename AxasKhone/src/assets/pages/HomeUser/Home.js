34f32fe91efa58ca2027e5b1f9a08a22c6e8379f
samy <samprada3@gmail.com>
2018-09-04 16:07:25 +0430

    home screen navigator set

---
+++ AxasKhone/src/assets/pages/HomeUser/Home.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class Home extends Component {
+  static navigationOptions = {
+    tabBarLabel: ({ tintColor }) => (
+      <Text style={{ color: tintColor, textAlign: 'center', marginBottom: 12 }}>
+        search
+      </Text>
+    )
+  };
+
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: 'rgb(239, 239, 239)',
+          justifyContent: 'center'
+        }}
+      >
+        <Text
+          style={{
+            textAlign: 'center',
+            Color: 'rgb(57, 57, 57)',
+            fontSize: 24
+          }}
+        >
+          under develop :))
+        </Text>
+      </View>
+    );
+  }
+}
