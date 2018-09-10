d16982b234f04e41e3b2b5f86d43c2ce1a48ba07
samy <samprada3@gmail.com>
2018-09-11 01:33:42 +0430

    search  page basic created

--- AxasKhone/src/assets/pages/Search/index.js
+++ AxasKhone/src/assets/pages/Search/index.js
@@ -1,35 +1,32 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import {
+  Container,
+  Header,
+  Item,
+  Input,
+  Icon,
+  Button,
+  Text,
+  Tab,
+  Tabs
+} from 'native-base';
 
 export default class Search extends Component {
-  static navigationOptions = {
-    tabBarLabel: ({ tintColor }) => (
-      <Text style={{ color: tintColor, textAlign: 'center', marginBottom: 12 }}>
-        search
-      </Text>
-    )
-    // tabBarIcon: return{ focused: boolean, tintColor: string }
-  };
-
   render() {
     return (
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: 'rgb(239, 239, 239)',
-          justifyContent: 'center'
-        }}
-      >
-        <Text
-          style={{
-            textAlign: 'center',
-            Color: 'rgb(57, 57, 57)',
-            fontSize: 24
-          }}
-        >
-          under develop :))
-        </Text>
-      </View>
+      <Container>
+        <Header searchBar rounded>
+          <Item>
+            <Icon name="ios-people" />
+            <Input placeholder="جستجو" />
+            <Icon name="ios-search" />
+          </Item>
+        </Header>
+        <Tabs>
+          <Tab heading="کاربران">{/* <Tab1 /> */}</Tab>
+          <Tab heading="هشتگ">{/* <Tab2 /> */}</Tab>
+        </Tabs>
+      </Container>
     );
   }
 }
