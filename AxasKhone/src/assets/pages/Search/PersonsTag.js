ea612b0152bb7097f9e1e7c67aad985886327e1f
Ehsan Maders <ehsantitsh@gmail.com>
2018-09-13 16:34:00 +0430

    update user search

--- AxasKhone/src/assets/pages/Search/PersonsTag.js
+++ AxasKhone/src/assets/pages/Search/PersonsTag.js
@@ -1,56 +1,112 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity, FlatList, Image } from 'react-native';
-import { Icon } from 'native-base';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
 
 export default class PersonsTag extends Component {
   constructor() {
     super();
     this.state = {
-      FavoriteItem: undefined
+      profilePic: undefined,
+      username: undefined,
+      fullname: undefined,
+      mode: undefined
     };
+  }
+
+  componentWillReceiveProps() {
+    const { person } = this.props;
+    this.setState({
+      profilePic: person.profile_picture,
+      username: person.main_username,
+      fullname: person.fullname,
+      mode: person.is_following
+    });
   }
 
   render() {
     return (
-      // const favoriteBox = this.props.favoriteBox;
-      // <View style={styles.favoriteContainer} />
-      <View
-        style={{
-          height: 50,
-          // backgroundColor: 'red',
-          flexDirection: 'row-reverse',
-          borderBottomColor: 'gray',
-          borderBottomWidth: 0.5
-        }}
-      >
+      <View>
         <View
           style={{
-            // backgroundColor: 'yellow',
-            justifyContent: 'center',
-            marginHorizontal: 5
+            height: 50,
+            flexDirection: 'row-reverse',
+            borderBottomColor: 'gray',
+            borderBottomWidth: 0.5
           }}
         >
-          <Icon type="FontAwesome" name="user" style={{ fontSize: 15 }} />
+          <View
+            style={{
+              justifyContent: 'center',
+              marginHorizontal: 5
+            }}
+          >
+            <Image
+              borderRadius={20}
+              style={{ width: 40, height: 40 }}
+              source={{ uri: `http://${this.state.profilePic}` }}
+            />
+          </View>
+          <View
+            style={{
+              flex: 2,
+              flexDirection: 'column',
+              paddingRight: 5,
+              justifyContent: 'center'
+            }}
+          >
+            <Text style={{ textAlign: 'right', fontSize: 15 }}>
+              {this.state.username}
+            </Text>
+            <Text style={{ textAlign: 'right', fontSize: 9, color: 'gray' }}>
+              {this.state.fullname}
+            </Text>
+          </View>
+          <View
+            style={{
+              flex: 2,
+              justifyContent: 'center',
+              alignContent: 'center'
+            }}
+          >
+            <TouchableOpacity activeOpacity={0.8}>
+              <View
+                style={{
+                  borderRadius: 5,
+                  textAlign: 'center',
+                  marginHorizontal: 15,
+                  padding: 5,
+                  backgroundColor: [
+                    this.state.mode === false ? 'green' : null,
+                    this.state.mode === true ? 'gray' : null
+                  ].filter(item => {
+                    if (item !== null) {
+                      return item;
+                    }
+                  })[0]
+                }}
+              >
+                <Text
+                  style={{
+                    alignContent: 'center',
+                    textAlign: 'center',
+
+                    color: 'white'
+                  }}
+                >
+                  {
+                    [
+                      this.state.mode === false ? 'دنبال کردن' : null,
+                      this.state.mode === true ? 'دنبال شده است' : null
+                    ].filter(item => {
+                      if (item !== null) {
+                        return item;
+                      }
+                    })[0]
+                  }
+                </Text>
+              </View>
+            </TouchableOpacity>
+          </View>
         </View>
-        <View
-          style={{
-            flex: 2,
-            flexDirection: 'column',
-            paddingRight: 5,
-            justifyContent: 'center'
-          }}
-        >
-          <Text style={{ textAlign: 'right', fontSize: 18 }}>
-            {this.props.person}
-          </Text>
-        </View>
-        <View
-          style={{
-            flex: 2,
-            justifyContent: 'center',
-            alignContent: 'center'
-          }}
-        />
       </View>
     );
   }
