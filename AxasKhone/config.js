72f58f127512dd173a7cbf09428286a752e982dd
Ehsan Maders <ehsantitsh@gmail.com>
2018-09-23 01:52:36 +0330

    clean config file

--- AxasKhone/config.js
+++ AxasKhone/config.js
@@ -1,8 +1,8 @@
 const apiServerIpIos = '127.0.0.1';
 const apiServerIpAndroid = '10.0.2.2';
-const apiServerIpProduction = '192.168.10.136';
+const apiServerIpProduction = '';
 const apiServerPort = 8000;
-const activeServer = 'server';
+const activeServer = 'android';
 
 const apiServerAddress = {
   android: `http://${apiServerIpAndroid}:${apiServerPort}`,
@@ -12,5 +12,7 @@ const apiServerAddress = {
     return `${this[activeServer.toString()]}`;
   }
 };
+
+export const storageSecretKey = '';
 
 export default apiServerAddress;
